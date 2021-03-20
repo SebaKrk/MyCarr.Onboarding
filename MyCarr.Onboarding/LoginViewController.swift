89309b4237ed3e05155b0d3a99f436bad8a8c071
SebaKrk <s.sciuba@gmail.com>
2021-03-20 15:37:32 +0100

    start project

--- MyCarr.Onboarding/LoginViewController.swift
+++ MyCarr.Onboarding/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MyCarr.Onboarding
 //
 //  Created by Sebastian Sciuba on 20/03/2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController : UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
-
