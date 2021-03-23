3edb141a5166261a4c1adafa200a9df530715a9b
SebaKrk <s.sciuba@gmail.com>
2021-03-23 14:03:06 +0100

    create a CostumRegistrationTF class

--- MyCarr.Onboarding/View/CostumRegistrationTF.swift
+++ MyCarr.Onboarding/View/CostumRegistrationTF.swift
@@ -1,18 +1,16 @@
 //
-//  CostumTextFields.swift
+//  CostumRegistrationTF.swift
 //  MyCarr.Onboarding
 //
-//  Created by Sebastian Sciuba on 22/03/2021.
+//  Created by Sebastian Sciuba on 23/03/2021.
 //
 
 import UIKit
 
- class CostumTextFields: UITextField {
+class CostumRegistrationTF : UITextField {
     
-    init (placeHolder: String, colorText: UIColor,bottomLinieColor: UIColor, isSecure: Bool? = false) {
-        
+    init(placeHolder : String) {
         super.init(frame: .zero)
-        translatesAutoresizingMaskIntoConstraints = false
         
         let spacer = UIView()
         spacer.heightAnchor.constraint(equalToConstant: 50).isActive = true
@@ -20,26 +18,20 @@ import UIKit
         leftView = spacer
         leftViewMode = .always
         
-        textColor = colorText
+        textColor = .primaryOrange()
         font = UIFont(name: "Sen-Regular", size: 16)
         placeholder = placeHolder
-        attributedPlaceholder = NSAttributedString(string: placeHolder ,attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.5)])
         heightAnchor.constraint(equalToConstant: 50).isActive = true
         
-
         let bottomLine = UIView()
         addSubview(bottomLine)
         bottomLine.translatesAutoresizingMaskIntoConstraints = false
-        bottomLine.backgroundColor = bottomLinieColor
+        bottomLine.backgroundColor = .inactiveGray()
         bottomLine.heightAnchor.constraint(equalToConstant: 2.0).isActive = true
         bottomLine.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor).isActive = true
         bottomLine.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor).isActive = true
         bottomLine.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        
-        isSecureTextEntry = isSecure!
-        
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
