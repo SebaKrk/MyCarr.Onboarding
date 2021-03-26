517a80b8e9b3bd39c34687e41cf37297beb2fabe
SebaKrk <s.sciuba@gmail.com>
2021-03-26 13:14:43 +0100

    refactor constraints and keyboard bug

--- MyCarr.Onboarding/Controllers/Authentication/Login/LoginViewController.swift
+++ MyCarr.Onboarding/Controllers/Authentication/Login/LoginViewController.swift
@@ -15,13 +15,13 @@ class LoginViewController : UIViewController {
     private var viewModel = LoginViewModel()
     
     let topView = UIView()
-    let scrollView = UIScrollView()
+    let centerView = UIView()
+    let centerView2 = UIView()
     let bottomView = UIView()
     
     private let titleLabel = CostumLabel(title: "Zaloguj się", size: 48, color: .white, line: 1)
     private let emailTextField = CostumTextFields( placeHolder: "enter your email adress", colorText: .white, bottomLinieColor: .white)
     private let passwordTextField = CostumTextFields(placeHolder: "enter your password", colorText: .white, bottomLinieColor: .white, isSecure: true)
-
     
     private let forgetPasswordButton: UIButton = {
         let button = UIButton()
@@ -76,19 +76,13 @@ class LoginViewController : UIViewController {
     
     override func viewWillLayoutSubviews() {
         configureGradientBackgraound()
-        setupConstraints()
+        setupView()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
         configureTextFieldObservers()
-    }
-    
-    //    MARK: - SetupView
-    
-    func configureUI() {
-        navigationController?.navigationBar.isHidden = true
-        navigationController?.navigationBar.barStyle = .black
+        setupConstraints()
     }
     
     //    MARK: - Action
@@ -126,96 +120,104 @@ class LoginViewController : UIViewController {
         }
         checkFormStatus()
     }
+    //    MARK: - SetupView
     
+    func setupView() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        view.addSubview(topView)
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        topView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        
+        view.addSubview(centerView)
+        centerView.translatesAutoresizingMaskIntoConstraints = false
+        centerView.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
+        centerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        centerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        centerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        
+        view.addSubview(centerView2)
+        centerView2.translatesAutoresizingMaskIntoConstraints = false
+        centerView2.topAnchor.constraint(equalTo: centerView.bottomAnchor).isActive = true
+        centerView2.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        centerView2.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        centerView2.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        
+        view.addSubview(bottomView)
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        bottomView.topAnchor.constraint(equalTo: centerView2.bottomAnchor).isActive = true
+        bottomView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        bottomView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        bottomView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+    }
     
     //    MARK: - SetupConstraints
     
     func setupConstraints() {
-        view.addSubview(topView)
-        topView.translatesAutoresizingMaskIntoConstraints = false
-        topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        topView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        topView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        topView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
         
         topView.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.centerYAnchor.constraint(equalTo: topView.centerYAnchor).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: topView.centerXAnchor).isActive = true
         
-        view.addSubview(scrollView)
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: 20).isActive = true;
-        scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5).isActive = true;
-        scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5).isActive = true;
-        scrollView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6).isActive = true
-        
         let stack = UIStackView(arrangedSubviews: [emailTextField,passwordTextField,forgetPasswordButton])
-        scrollView.addSubview(stack)
-    
-        stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
-        stack.spacing = 15
+        stack.spacing = 10
         
-        stack.topAnchor.constraint(equalTo: scrollView.topAnchor,constant: 10).isActive = true
-        stack.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        stack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor,constant: 50).isActive = true
-        stack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor,constant: -50).isActive = true
-        stack.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-  
+        centerView.addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints =  false
+        stack.leftAnchor.constraint(equalTo: centerView.leftAnchor,constant: 46).isActive = true
+        stack.rightAnchor.constraint(equalTo: centerView.rightAnchor,constant: -46).isActive = true
+        stack.bottomAnchor.constraint(equalTo: centerView.bottomAnchor).isActive = true
+        
         let stack2 = UIStackView(arrangedSubviews: [loginButton,registerButton])
         stack2.axis = .vertical
-        stack2.spacing = 5
-
         
-        scrollView.addSubview(stack2)
+        centerView2.addSubview(stack2)
         stack2.translatesAutoresizingMaskIntoConstraints = false
-        stack2.topAnchor.constraint(equalTo: stack.bottomAnchor,constant: 50).isActive = true
-        stack2.leftAnchor.constraint(equalTo: stack.leftAnchor).isActive = true
-        stack2.rightAnchor.constraint(equalTo: stack.rightAnchor).isActive = true
-
-
+        stack2.centerYAnchor.constraint(equalTo: centerView2.centerYAnchor).isActive = true
+        stack2.leftAnchor.constraint(equalTo: centerView2.leftAnchor,constant: 40).isActive = true
+        stack2.rightAnchor.constraint(equalTo: centerView2.rightAnchor,constant: -40).isActive = true
+        
         let stack3 = UIStackView(arrangedSubviews: [textLabel,mailButton,facebookButton,gmailButton])
         stack3.axis = .horizontal
         stack3.spacing = 10
-
-        scrollView.addSubview(stack3)
-        stack3.translatesAutoresizingMaskIntoConstraints = false
-        stack3.topAnchor.constraint(equalTo: stack2.bottomAnchor,constant: 50).isActive = true
-        stack3.leftAnchor.constraint(equalTo: stack.leftAnchor).isActive = true
-        stack3.rightAnchor.constraint(equalTo: stack.rightAnchor).isActive = true
         
-        view.addSubview(bottomView)
-        bottomView.translatesAutoresizingMaskIntoConstraints = false
-        bottomView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        bottomView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        bottomView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        bottomView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.08).isActive = true
+        bottomView.addSubview(stack3)
+        stack3.translatesAutoresizingMaskIntoConstraints = false
+        stack3.topAnchor.constraint(equalTo: bottomView.topAnchor).isActive = true
+        stack3.leftAnchor.constraint(equalTo: bottomView.leftAnchor,constant: 46).isActive = true
+        stack3.rightAnchor.constraint(equalTo: bottomView.rightAnchor,constant: -46).isActive = true
         
         bottomView.addSubview(logoImageLabel)
         logoImageLabel.translatesAutoresizingMaskIntoConstraints = false
-        logoImageLabel.centerYAnchor.constraint(equalTo: bottomView.centerYAnchor).isActive = true
         logoImageLabel.centerXAnchor.constraint(equalTo: bottomView.centerXAnchor).isActive = true
+        logoImageLabel.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor).isActive = true
     }
     
     // MARK: - Helpers
-        
-        func checkFormStatus() {
-            if viewModel.formIsValid {
-                loginButton.isEnabled = true
-                loginButton.backgroundColor = .white
-                loginButton.alpha = 1
-                loginButton.setTitleColor(.inactiveGray(), for: .normal)
-            } else {
-                loginButton.isEnabled = false
-                loginButton.backgroundColor = .white
-                loginButton.alpha = 0.5
-                loginButton.setTitleColor(.primaryOrange(), for: .normal)
-            }
+    
+    func checkFormStatus() {
+        if viewModel.formIsValid {
+            loginButton.isEnabled = true
+            loginButton.backgroundColor = .white
+            loginButton.alpha = 1
+            loginButton.setTitleColor(.inactiveGray(), for: .normal)
+        } else {
+            loginButton.isEnabled = false
+            loginButton.backgroundColor = .white
+            loginButton.alpha = 0.5
+            loginButton.setTitleColor(.primaryOrange(), for: .normal)
         }
-        
-        func configureTextFieldObservers() {
-            emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
-            passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
-        }
+    }
+    
+    func configureTextFieldObservers() {
+        emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
 }
