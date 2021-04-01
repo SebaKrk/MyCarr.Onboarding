da210ad63d85c624156897bb0ebb88fa3de3c7ae
SebaKrk <s.sciuba@gmail.com>
2021-04-01 19:31:32 +0200

    create ConversationTypeVC

--- MyCarr.Onboarding/Controllers/Main/SideMenu/HelpCenter/ConversationTypeVC.swift
+++ MyCarr.Onboarding/Controllers/Main/SideMenu/HelpCenter/ConversationTypeVC.swift
@@ -1,51 +1,42 @@
 //
-//  HelpCenterViewController.swift
+//  ConversationTypeVC.swift
 //  MyCarr.Onboarding
 //
 //  Created by Sebastian Sciuba on 01/04/2021.
 //
 
+
 import UIKit
 
-class HelpCenterViewController : UIViewController {
+class ConversationTypeVC : UIViewController {
     
     let topView = UIView()
     let secondView = UIView()
     
-    private let titleLabel = CostumLabel(title: "Potrzebujesz\npomocy?", size: 40, color: .black, line: 2)
+    private let titleLabel = CostumLabel(title: "Kontakt\nprzez?", size: 40, color: .black, line: 2)
     
-    private let faqImage = CostumSmallImage(imageName: "FAQ")
-    private let faqLabel = CostumLabel(title: "Centrum pomocy", size: 16, color: .black, line: 1)
-    private  let faqButton : UIButton = {
+    private let phoneImage = CostumSmallImage(imageName: "phonecall")
+    private let phoneLabel = CostumLabel(title: "Rozmowa tefoniczna", size: 16, color: .black, line: 1)
+    private  let phoneButton : UIButton = {
         let button = UIButton()
         button.setImage(#imageLiteral(resourceName: "Forward"), for: .normal)
-        button.addTarget(self, action: #selector(handleFAQ), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handlePhoneButton), for: .touchUpInside)
         button.widthAnchor.constraint(equalToConstant: 50).isActive = true
         return button
     }()
     
-    private let helpCenterImage = CostumSmallImage(imageName: "HelpCenter")
-    private let helpCenterLabel = CostumLabel(title: "Centrum pomocy", size: 16, color: .black, line: 1)
-    private  let helpCenterButton : UIButton = {
+    private let chatImage = CostumSmallImage(imageName: "message")
+    private let chatILabel = CostumLabel(title: "Chat", size: 16, color: .black, line: 1)
+    private  let chatIButton : UIButton = {
         let button = UIButton()
         button.setImage(#imageLiteral(resourceName: "Forward"), for: .normal)
-        button.addTarget(self, action: #selector(handleHelpButton), for: .touchUpInside)
-        button.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        return button
-    }()
-    
-    private let historyImage = CostumSmallImage(imageName: "History")
-    private let historyLabel = CostumLabel(title: "Historia Zgloszeń", size: 16, color: .black, line: 1)
-    private  let historyButton : UIButton = {
-        let button = UIButton()
-        button.setImage(#imageLiteral(resourceName: "Forward"), for: .normal)
-        button.addTarget(self, action: #selector(handleHistoryButton), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handlechatIButton), for: .touchUpInside)
         button.widthAnchor.constraint(equalToConstant: 50).isActive = true
         return button
     }()
     
     private let dividerView1 = DividerView()
-    private let dividerView2 = DividerView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,23 +48,18 @@ class HelpCenterViewController : UIViewController {
     
     //    MARK: - Action
     
-    @objc func handleFAQ() {
-        print("DEBUG: FAQ")
-    }
-    @objc func handleHelpButton() {
-        print("DEBUG: Help Center")
-        let rootVC = ConversationTypeVC()
-        let navVC = UINavigationController(rootViewController: rootVC)
-        navVC.modalPresentationStyle = .fullScreen
-        present(navVC, animated: true )
-    }
-    @objc func handleHistoryButton() {
-        print("DEBUG: Hotsoty")
-    }
-    @objc func handleBackButton() {
-        dismiss(animated: true, completion: nil)
+    @objc func handlePhoneButton () {
+        print("phone button pressed")
     }
     
+    @objc func handlechatIButton () {
+        print("chat button pressed")
+    }
+    
+    @objc func handleBackButton () {
+        print("back button pressed")
+        dismiss(animated: true, completion: nil)
+    }
     
     //    MARK: - SetUpNavigationBar
     
@@ -93,7 +79,7 @@ class HelpCenterViewController : UIViewController {
         topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         topView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         topView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        topView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15).isActive = true
+        topView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
         
         view.addSubview(secondView)
         secondView.translatesAutoresizingMaskIntoConstraints = false
@@ -104,29 +90,23 @@ class HelpCenterViewController : UIViewController {
         
     }
     
-    //    MARK: - SetUpConstraints
-    
+//    MARK: - SetupConstraints
     
     func setupConstraints() {
-        
         topView.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.topAnchor.constraint(equalTo: topView.topAnchor,constant: 10).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: topView.centerXAnchor).isActive = true
         titleLabel.leftAnchor.constraint(equalTo: topView.leftAnchor,constant: 42).isActive = true
         
-        let stack1 = UIStackView(arrangedSubviews: [faqImage,faqLabel,faqButton])
+        let stack1 = UIStackView(arrangedSubviews: [phoneImage,phoneLabel,phoneButton])
         stack1.axis = .horizontal
         stack1.spacing = 10
-        
-        let stack2 = UIStackView(arrangedSubviews: [helpCenterImage,helpCenterLabel,helpCenterButton])
+    
+        let stack2 = UIStackView(arrangedSubviews: [chatImage,chatILabel,chatIButton])
         stack2.axis = .horizontal
         stack2.spacing = 10
         
-        let stack3 = UIStackView(arrangedSubviews: [historyImage,historyLabel,historyButton])
-        stack3.axis = .horizontal
-        stack3.spacing = 10
-        
-        let mainStackView = UIStackView(arrangedSubviews: [stack1, dividerView1, stack2 , dividerView2, stack3])
+        let mainStackView = UIStackView(arrangedSubviews: [stack1, dividerView1, stack2])
         mainStackView.axis = .vertical
         mainStackView.spacing = 30
         
@@ -134,7 +114,7 @@ class HelpCenterViewController : UIViewController {
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
         mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 46).isActive = true
         mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -46).isActive = true
-        mainStackView.centerYAnchor.constraint(equalTo: secondView.centerYAnchor).isActive = true
-        mainStackView.centerXAnchor.constraint(equalTo: secondView.centerXAnchor).isActive = true
+        mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 }
